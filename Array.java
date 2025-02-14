11e2916f5e4bd24ba2e922f66ba2e026a7b1a28d
ruixiliulouis <louisliurx@gmail.com>
2025-02-14 13:01:31 -0800

    complete

--- Array.java
+++ Array.java
@@ -7,6 +7,15 @@ public class Array
 	// 7 8 9
 	public int[][] getRowMajorSquare(int x)
 	{
+		int count = 0; 
+		int [][] a = new int [x][x];
+		for(int row= 0; row< x; row++){
+			for( int col=0 ; col<x; col++){
+				a[row][col]= count;
+				count ++;
+			}
+		}
+		return a;
 //	
 	}
 
@@ -15,8 +24,16 @@ public class Array
 	// 2 5 8
 	// 3 6 9
 	public int[][] getColumnMajorSquare(int x)
-	{
-//		
+	{	
+		int count = 0; 
+		int [][] a = new int [x][x];
+		for(int row= 0; row< x; row++){
+			for( int col=0 ; col<x; col++){
+				a[row][col]= count;
+			}
+			count ++;
+		}
+		return a;
 	}
 
 	// Array should be of format
@@ -25,7 +42,15 @@ public class Array
 	// 9 10 11 12
 	public int[][] getRowMajorRectangle(int row, int column)
 	{
-		
+		int count = 0; 
+		int [][] a = new int [row][column];
+		for(int i= 0; i< row; i++){
+			for( int j=0 ; j<column; j++){
+				a[i][j]= count;
+				count ++;
+			}
+		}
+		return a;
 	}
 
 	// Array should be of format
@@ -34,7 +59,16 @@ public class Array
 	// 3 6 9 12
 	public int[][] getColumnMajorRectangle(int column, int row)
 	{
-	
+		int count = 0; 
+		int [][] a = new int [row][column];
+		for(int i= 0; i< row; i++){
+			for( int j=0 ; j<column; j++){
+				a[i][j]= count;
+				
+			}
+			count ++;
+		}
+		return a;
 	}
 
 	// Please note that each digit occupies two spaces and there is a space between
@@ -42,7 +76,12 @@ public class Array
 	// You are not required to deal with three digit numbers.
 	public void printDouble(int[][] a)
     {
-	
+		for (int i = 0; i < a.length; i++) {
+			for (int j = 0; j < a[i].length; j++) {
+				System.out.printf("%2d ", a[i][j]); 
+			}
+			System.out.println(); 
+		}
 
 	}
 }
